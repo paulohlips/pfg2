ac5b0d8fb0abcb1b8b198229100164c6601eedf6
Paulo Henrique Ferreira <paulohfcmendes@gmail.com>
2019-11-19 17:40:36 -0300

    first commit

---
+++ scraper/src/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import gplay from "google-play-scraper";
+import util from "util";
+import ApkReader from "node-apk-parser";
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/app:keyword", (req, res) => {
+  const keyword = req.params.keyword;
+
+  gplay
+    .search({
+      term: keyword,
+      num: 2
+    })
+    .then(resolve => {
+      return res.status(200).json({
+        resultado: resolve
+      });
+    });
+});
+
+server.post("/permission", (req, res) => {
+  const appid = Object.values(req.body);
+
+  gplay.permissions({ appId: appid[0] }).then(resolve => {
+    return res.status(200).json({
+      resultado: resolve
+    });
+  });
+});
+
+server.post("/apk", (req, res) => {
+  const path = Object.values(req.body);
+
+  const reader = ApkReader.readFile(path[0]);
+  const manifest = reader.readManifestSync();
+
+  return res.status(200).json({ permissions: manifest.usesPermissions });
+});
+
+server.listen(3001);
